1d36987ed59597919f8c10ba6886d46ca39339b8
samy <samy_leni@hotmail.fr>
2019-05-12 04:15:34 +0200

    add:options

--- js/canvas.js
+++ js/canvas.js
@@ -1,14 +1,17 @@
 class Canvas {
-    constructor(parent, gradient) {
+    constructor(parent, gradient, zoom, width, height) {
         this.canvas = document.createElement('canvas');
         this.canvas.id = 'canvas';
         this.cx = this.canvas.getContext('2d');
-        this.canvas.width = 512;
-        this.canvas.height = 512;
+        this.zoom = zoom;
+        this.canvas.width = width * this.zoom;
+        this.canvas.height = height * this.zoom;
         parent.appendChild(this.canvas);
-    
+        this.cx.scale(this.zoom, this.zoom);
+
         this.particles = [];
         this.gradient = gradient;
+        this.roughness;
 
         this.calculPoint = (radius, blur) => {
             var point = document.createElement('canvas');
@@ -44,33 +47,33 @@ class Canvas {
         }
 
         this.draw = () => {
-            var gradient = this.calculGradient(this.gradient);
-
             this.cx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
             this.particles.forEach(particle => {
                 var point = this.calculPoint(particle.radius, particle.blur);
                 particle.intensity >= particle.volatility ? particle.intensity -= particle.volatility : particle.intensity = 0;
-                
                 this.cx.globalAlpha = Math.min(particle.intensity, 1);
                 this.cx.drawImage(point, particle.pos.x - point.radius, particle.pos.y - point.radius);
             });
+            this.particles = this.particles.filter(particle => particle.intensity > 0);
             
+            var gradient = this.calculGradient(this.gradient);
             var colored = this.cx.getImageData(0, 0, this.canvas.width, this.canvas.height);
             this.colorize(colored.data, gradient);
             this.cx.putImageData(colored, 0, 0);
-
-            this.particles = this.particles.filter(particle => particle.intensity > 0);
         }
 
         this.colorize = (pixels, gradient) => {
-            for (var i = 0, len = pixels.length, j; i < len; i += 4) {
+            for (var i = 0, j; i < pixels.length; i += 4) {
                 j = pixels[i + 3] * 4;
-
                 if (j) {
-                    pixels[i] = gradient[j];
-                    pixels[i + 1] = gradient[j + 1];
-                    pixels[i + 2] = gradient[j + 2];
+                    if ((Math.floor((i / 4 % this.canvas.width) / this.zoom) % 2  === Math.floor(Math.floor(i / 4 / this.canvas.height) / this.zoom) % 2) && j / 4 < 128) pixels[i + 3] = 0;
+                    else {
+                        pixels[i] = gradient[j];
+                        pixels[i + 1] = gradient[j + 1];
+                        pixels[i + 2] = gradient[j + 2];
+                        if (pixels[i + 3] > this.roughness) pixels[i + 3] = 255;
+                    }
                 }
             }
         }
--- js/main.js
+++ js/main.js
@@ -1,31 +1,45 @@
+var zoom = 4;
+var width = 128;
+var height = 128;
+var gradient = {
+    0.0: 'black',
+    0.5: 'blue',
+    0.6: 'cyan',
+    0.8: 'lime',
+    0.9: 'yellow',
+    1.0: 'white'
+}
+var monochromeGradient = {
+    1.0: '#fff'
+}
+
 window.onload = () => {
-    var posX = posY = null;
+    var display = new Canvas(document.body, gradient, zoom, width, height);
 
-    var gradient = {
-        0.5: 'blue',
-        0.6: 'cyan',
-        0.8: 'lime',
-        0.9: 'yellow',
-        1.0: 'white'
-    }
-
-    var display = new Canvas(document.body, gradient);
-
-    document.getElementById('canvas').onmousemove = event => {
-        posX = event.layerX;
-        posY = event.layerY;
-    }
-    document.getElementById('canvas').onmouseleave = event => posX = posY = null;
+    var step = 0;
+    var posX = width / 4;
+    var posY = height / 2;
 
     var frame = () => {
-        if (posX && posY) display.particles.push(new Particle(
-            new Vector2D(posX, posY),
+        if (display.zoom !== zoom) {
+            display.zoom = zoom;
+            display.canvas.width = width * display.zoom;
+            display.canvas.height = height * display.zoom;
+            display.cx.scale(display.zoom, display.zoom);
+        }
+        display.gradient = document.getElementById("monochrome").checked ? monochromeGradient : gradient;
+        display.roughness = document.getElementById("roughness").value;
+        display.particles.push(new Particle(
+            new Vector2D(
+                posX += Math.sin(step * 0.047) * 1.5,
+                posY += Math.cos(step * 0.047) * 1.5),
             parseInt(document.getElementById("radius").value),
             parseInt(document.getElementById("blur").value),
             parseInt(document.getElementById("intensity").value) / 100,
             parseInt(document.getElementById("volatility").value) / 100));
         display.draw();
+        step++;
         requestAnimationFrame(frame);
     }
-    requestAnimationFrame(frame)
+    requestAnimationFrame(frame);
 }
